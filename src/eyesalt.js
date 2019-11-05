c5ac47c61d78d9a9e14132fe0432b8ade95d3fa5
Spenser Black <spenserblack01@gmail.com>
2019-11-05 10:21:44 -0500

    Use themer

---
+++ src/eyesalt.js
@@ -0,0 +1,14 @@
+module.exports.colors = {
+  dark: {
+    accent0: '#AF5F00',
+    accent1: '#FFAF87',
+    accent2: '#AF9E00',
+    accent3: '#ABAF00',
+    accent4: '#FFD7D7',
+    accent5: '#FFAF5F',
+    accent6: '#FFD7AF',
+    accent7: '#D78787',
+    shade0: '#5E3F3F',
+    shade7: '#230000',
+  },
+};
